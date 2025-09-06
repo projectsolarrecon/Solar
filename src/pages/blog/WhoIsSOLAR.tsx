543fd06b7348945a3e09bd2df5df49e86af2422e
projectsolarrecon <projectsolarrecon@gmail.com>
2025-09-06 10:11:08 -0400

    Update WhoIsSOLAR.tsx

--- src/pages/blog/WhoIsSOLAR.tsx
+++ src/pages/blog/WhoIsSOLAR.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import SEO from '../../../components/SEO';
+import SEO from '../../components/SEO';
 
 export default function WhoIsSOLAR(): JSX.Element {
   return (
