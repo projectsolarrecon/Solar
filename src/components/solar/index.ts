62548d9dab8e5f40edab1fcaaa03da9395ef356c
projectsolarrecon <projectsolarrecon@gmail.com>
2025-09-11 13:30:23 -0400

    Update index.ts

--- src/components/solar/index.ts
+++ src/components/solar/index.ts
@@ -1,3 +1,12 @@
-export { default as BandHeader } from "./BandHeader";
+export { default as SectionBand } from "./SectionBand";
+export { default as SectionCard } from "./SectionCard";
 export { default as Callout } from "./Callout";
+export { default as Checklist } from "./Checklist";
+export { default as TOC } from "./TOC";
+export { default as SourcesOffline } from "./SourcesOffline";
+
+// You already have these (keep them exported if present)
+export { default as ShareBar } from "./ShareBar";
+export { default as BandHeader } from "./BandHeader";
 export { default as PullQuote } from "./PullQuote";
+export { default as SourceList } from "./SourceList";
