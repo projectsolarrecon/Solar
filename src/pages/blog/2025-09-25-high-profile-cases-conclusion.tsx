cbacaa49c71383fe655aed2c7e12b7f2806a9eb5
projectsolarrecon <projectsolarrecon@gmail.com>
2025-09-24 22:25:11 -0400

    Update 2025-09-25-high-profile-cases-conclusion.tsx

--- src/pages/blog/2025-09-25-high-profile-cases-conclusion.tsx
+++ src/pages/blog/2025-09-25-high-profile-cases-conclusion.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import BlogLayout from "../../../../components/layouts/BlogLayout";
+import BlogLayout from "../../components/layouts/BlogLayout";
 import {
   BandHeader,
   Callout,
   PullQuote,
   Divider,
-} from "../../../../components/solar";
+} from "../../components/solar";
 
 const linkCls =
   "text-blue-700 underline underline-offset-2 hover:text-blue-900";
