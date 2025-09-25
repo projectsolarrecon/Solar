941d33f154e9d1a6ccd80d4379b9f252d198754c
projectsolarrecon <projectsolarrecon@gmail.com>
2025-09-24 21:46:43 -0400

    add blog post: The Registry Is Working Exactly as Designed (2025-09-25)

---
+++ src/pages/blog/2025-09-25-high-profile-cases-conclusion/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import BlogLayout from "../../../../components/layouts/BlogLayout";
+import {
+  BandHeader,
+  Callout,
+  PullQuote,
+  Divider,
+} from "../../../../components/solar";
+
+const linkCls = "text-blue-700 underline underline-offset-2 hover:text-blue-900";
+
+export default function HighProfileCasesConclusion(): JSX.
