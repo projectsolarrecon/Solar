5ff953bc2446f4092513855bbd7913ca81060584
projectsolarrecon <projectsolarrecon@gmail.com>
2026-08-25 15:28:24 -0400

    Route Supported Claims Guide

--- src/App.tsx
+++ src/App.tsx
@@ -52,6 +52,7 @@ import ResourceGuideSandbox from './pages/resources/ResourceGuideSandbox';
 import RiskAssessmentGuide from './pages/resources/RiskAssessmentGuide';
 import RecidivismEvidenceGuide from './pages/resources/RecidivismEvidenceGuide';
 import RegistryEffectivenessEvidenceGuide from './pages/resources/RegistryEffectivenessEvidenceGuide';
+import SupportedClaimsGuide from './pages/resources/SupportedClaimsGuide';
 import RegistryComplianceVerificationGuide from './pages/resources/RegistryComplianceVerificationGuide';
 import PoliceRegistryCPSEncountersGuide from './pages/resources/PoliceRegistryCPSEncountersGuide';
 import HousingCrisisHomelessnessDisasterSurvivalGuide from './pages/resources/HousingCrisisHomelessnessDisasterSurvivalGuide';
@@ -178,6 +179,7 @@ function App() {
               <Route path="/resources/risk-assessment-guide" element={<RiskAssessmentGuide />} />
               <Route path="/resources/recidivism-evidence-guide" element={<RecidivismEvidenceGuide />} />
               <Route path="/resources/registry-effectiveness-evidence-guide" element={<RegistryEffectivenessEvidenceGuide />} />
+              <Route path="/resources/supported-claims-guide" element={<SupportedClaimsGuide />} />
               <Route path="/resources/registry-compliance-verification-guide" element={<RegistryComplianceVerificationGuide />} />
               <Route path="/resources/police-registry-cps-encounters" element={<PoliceRegistryCPSEncountersGuide />} />
               <Route path="/resources/housing-crisis-homelessness-disaster-survival-guide" element={<HousingCrisisHomelessnessDisasterSurvivalGuide />} />
